02a2fa3225f4c726fa4f82ca450cde89ad6d18c3
programpacific <programpacific@users.noreply.github.com>
2018-07-08 12:54:00 -0600

    Add project files.

---
+++ CSharpBooleanWhileAndDowWhileConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBooleanWhileAndDowWhileConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
